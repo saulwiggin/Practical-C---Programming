9ae7c9f8719da6533559b46f19add67ffcfa6403
Saul Wiggin <saulwiggin@googlemail.com>
2014-10-01 10:09:25 +0100

    math program using std::cout

---
+++ tterm.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int term;	// term used in two expressions
+int main()
+{
+	term = 3*5;
+	std::cout << "Twice" << term << "is " << 2*term << "\n";
+	std::cout << "Three times " << term << " is " << 3*term << "\n";
+	return (0);
+}
